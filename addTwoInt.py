861c016298757f2f0d8bbc5248d6dab1cf554467
Iven022 <ivenmoonienxd@gmail.com>
2020-03-10 20:09:53 +0400

    Error messages updated

--- addTwoInt.py
+++ addTwoInt.py
@@ -3,5 +3,11 @@ def add(x,y):
 	return int(x)+int(y)
 
 if __name__=="__main__":
-	print(sys.argv)
-	print(add(sys.argv[1],sys.argv[2]))
+	if (len(sys.argv)==3):
+		print(add(sys.argv[1],sys.argv[2]))
+	elif (len(sys.argv)>3):
+		print("Too much values entered")
+	else:
+		print("Not enough values entered")
+
+
