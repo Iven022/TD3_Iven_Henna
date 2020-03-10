94f4a62624dc96488af86da6efd5f4cce9e2dd0e
Iven022 <ivenmoonienxd@gmail.com>
2020-03-10 20:29:25 +0400

    Asking user to add more values

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,13 +1,20 @@
 import sys
 def add(x,y):
-	return int(x)+int(y)
+  	return int(x)+int(y)
 
 if __name__=="__main__":
-	if (len(sys.argv)==3):
-		print(add(sys.argv[1],sys.argv[2]))
-	elif (len(sys.argv)>3):
-		print("Too much values entered")
-	else:
-		print("Not enough values entered")
+	try:
+		if (len(sys.argv)>3):
+			print("Too many values Entered")
+		else:
+			print(add(sys.argv[1],sys.argv[2]))
+	except Exception as e:
+		print("Not Enough Values Entered")
+		x=len(sys.argv)
+		CopyArg=sys.argv
+		while(x<3):
+			CopyArg.append(int(input("Enter value number " + str(x) + "    ")))
+			x+=x
+		print(add(CopyArg[1],CopyArg[2]))
 
 
