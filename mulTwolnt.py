8b36593d1cab101c5c06916e7e9d64118f8d0a1d
henna19 <hennarahman4@outlook.com>
2020-03-10 21:03:37 +0400

    commit No13

--- mulTwolnt.py
+++ mulTwolnt.py
@@ -9,4 +9,11 @@ if __name__ == "__main__":
 		else:
 			print(mul(sys.argv[1],sys.argv[2]))
 	except Exception as e:
-		print("Pas assez de valeur. Inserez deux valeur")
+		print("Inserez 2 valeur")
+		i=len(sys.argv)
+		CopyArg=sys.argv
+		while(i<3):
+			CopyArg.append(int(input("Valeurs " + str(i) + ": ")))
+			i+=i
+		print(mul(CopyArg[1],CopyArg[2]))
+
