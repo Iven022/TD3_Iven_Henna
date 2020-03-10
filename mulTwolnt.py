4e773e67f5f534525df2a3095f46086cc1ed3eeb
henna19 <hennarahman4@outlook.com>
2020-03-10 20:25:51 +0400

    commit pour no11

--- mulTwolnt.py
+++ mulTwolnt.py
@@ -3,5 +3,10 @@ def mul(a,b):
 	return int(a)*int(b)
 
 if __name__ == "__main__":
-	print(sys.argv)
-	print(mul(sys.argv[1],sys.argv[2]))
+	try:
+		if (len(sys.argv)>3):
+			print("Inserez que deux valeur")
+		else:
+			print(mul(sys.argv[1],sys.argv[2]))
+	except Exception as e:
+		print("Pas assez de valeur. Inserez deux valeur")
