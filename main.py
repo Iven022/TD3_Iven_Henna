1a13ccecf1e4dd5198b05a854ef36596bbe153da
Iven022 <ivenmoonienxd@gmail.com>
2020-03-11 14:47:35 +0400

    Last version

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
-<<<<<<< HEAD
 
 from addTwoInt import add
+from mulTwolnt import mul
 
-x="y"
-while((x=="y") or (x=="q")):
-	x=input("For Addition of two numbers press < y > or to quit press < q >" + "  ")
-	if (x=="y"):
+x="a"
+while((x=="a") or (x=="q") or (x=="b")):
+	print("For Addition of two numbers press < a > ")
+	print("For multiplacation press < m > ")
+	print("For both calculations press < b > ")
+	print("To quit press < q > ")
+	x=input("Enter desired process ")
+	if ((x=="a") or (x=="m") or (x=="b")):
 		try:
-			a=int(input("Enter first number"))
-			b=int(input("Enter Second number"))
-			print("Your result is: " + str(add(a,b)))
+			a=int(input("Enter first number   "))
+			b=int(input("Enter Second number   "))
+			if (x=="a"):
+				print("Your Addition  result is: " + str(add(a,b)))
+			elif (x=="m"):
+				print("Your Multiplication result is " + str(mul(a,b)))
+				x=="m"
+			else:
+				print("Your Addition  result is: " + str(add(a,b)))
+				print("Your Multiplication result is " + str(mul(a,b)))
+				x=="a"
+			print("  ")
+			print("  ")
+			z=input("More Calculations ? Press < y > for more or < n > to stop ")
+			if (z=="y"):
+				x=="a"
+			elif(z=="n"):
+				break
+			else:
+				print("Invalid input")
+				break
+
 		except Exception as e:
 			print("Invalid Input, try again")
 	elif (x=="q"):
 		break
 	else:
 		print("Invalid input, try again")
-		x="y"
+		x="a"
 
 
-
-
-
-=======
-from mulTwolnt import mul
-
-a="y"
-while((a=="y") or (a=="q")):
-	a=input("Pour multiplier appuyez sur la touch y, pour quitter appuyez q" + " ")
-	if (a=="y"):
-		try:
-			a=int(input("Entrez la valeur 1: "))
-			b=int(input("Entrez la valeur 2: "))
-			print("Le resultas est: " + str(mul(a,b)))
-		except Exception as e:
-			print("Entrez invalide")
-	elif (a=="q"):
-		break
-	else:
-		print("Entrez invalide")
-		a="y"
->>>>>>> a21b9efca9f907198f26f5c28e6bf8175463ab57
